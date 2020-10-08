cec4d6a90c5a978342eb4c68c3c0ade8c371fed6
KamilAdd-Byte <kamillodzinski09@gmail.com>
2020-10-08 20:19:38 +0200

    Change properties

--- springmysql/src/main/java/com/responsywnie/springmysql/model/Book.java
+++ springmysql/src/main/java/com/responsywnie/springmysql/model/Book.java
@@ -1,8 +1,53 @@
 package com.responsywnie.springmysql.model;
 
-public class Book {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Book {
+    private Long id;
     private String title;
     private String author;
     private int localization;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public Book() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getLocalization() {
+        return localization;
+    }
+
+    public void setLocalization(int localization) {
+        this.localization = localization;
+    }
+
 }
+
